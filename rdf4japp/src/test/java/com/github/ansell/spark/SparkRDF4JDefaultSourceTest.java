9ea9d2ec59a65c64558e1c375de940dea180e817
Peter Ansell <p_ansell@yahoo.com>
2016-05-19 01:30:29 -0400

    Add stub test

---
+++ rdf4japp/src/test/java/com/github/ansell/spark/SparkRDF4JDefaultSourceTest.java
@@ -0,0 +1,44 @@
+package com.github.ansell.spark;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SparkRDF4JDefaultSourceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testShortName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCreateRelationSQLContextMapOfStringStringStructType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCreateRelationSQLContextMapOfStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
