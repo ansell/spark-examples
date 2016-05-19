f5035a1f0d1ca0e4f9bec2a66126100e8d2a3bf7
Peter Ansell <p_ansell@yahoo.com>
2016-05-19 19:52:12 -0400

    Get testShortName working

--- rdf4japp/src/test/java/com/github/ansell/spark/SparkRDF4JDefaultSourceTest.java
+++ rdf4japp/src/test/java/com/github/ansell/spark/SparkRDF4JDefaultSourceTest.java
@@ -2,8 +2,11 @@ package com.github.ansell.spark;
 
 import static org.junit.Assert.*;
 
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.SQLContext;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,30 +16,33 @@ import org.junit.Test;
 
 public class SparkRDF4JDefaultSourceTest {
 
-	private static Repository repository;
+	private static SQLContext sqlContext;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		repository = new SailRepository(new MemoryStore());
-		repository.initialize();
+		sqlContext = new SQLContext(new SparkContext("local[2]", "SparkRDF4JDefaultSourceTest"));
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		repository.shutDown();
+		sqlContext.sparkContext().stop();
 	}
+
+	private String testQuery;
 
 	@Before
 	public void setUp() throws Exception {
+		testQuery = "select DISTINCT ?s where { GRAPH <http://bio2rdf.org/hgnc_resource:bio2rdf.dataset.hgnc.R3> { ?s a <http://bio2rdf.org/hgnc.symbol_vocabulary:Resource> . } } LIMIT 100";
 	}
 
 	@After
 	public void tearDown() throws Exception {
+
 	}
 
 	@Test
 	public final void testShortName() {
-		fail("Not yet implemented"); // TODO
+		assertEquals("spark-rdf4j-sparql", new SparkRDF4JDefaultSource().shortName());
 	}
 
 	@Test
